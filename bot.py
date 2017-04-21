f0804579021abac2cbec958790eadfa7d7f2d097
Tostapunk <s.mattia2000@gmail.com>
2017-04-21 22:31:51 +0200

    Added start and help functions

--- bot.py
+++ bot.py
@@ -13,6 +13,35 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+def start(bot, update):
+    text = """Welcome to PC-Control bot.
+Use /help to see all the commands!
+
+Made by <a href='http://www.t.me/Tostapunk'>Tostapunk</a>
+<a href='https://twitter.com/Schiavon_Mattia'>Twitter</a> | <a href='https://plus.google.com/+MattiaSchiavon'>Google+</a>"""
+    bot.sendMessage(chat_id=update.message.chat.id, text=text, parse_mode=ParseMode.HTML
+                    , disable_web_page_preview="true")
+
+def help(bot, update):
+    text = """<b>Available commands:</b>
+    /shutdown - To shutdown your PC
+    /reboot - To reboot your PC
+    /logout - To log out from your current account
+    /hibernate - To hibernate your PC
+    /cancel - To annul the previous command
+    /check - To check the PC status
+    /launch - To launch a program | Example: /launch notepad
+    /link - To open a link | Example: /link http://google.com (don't use "www")
+    /memo - To show a memo on your pc
+    /task - To check if a process is currently running | Example: /task chrome
+    /screen - To take a screenshot and receive it through Imgur
+
+You can set a delay time for the execution of the first four commands by using _t + time in seconds after a command.
+    Example: /shutdown_t 20"""
+    bot.sendMessage(chat_id=update.message.chat.id, text=text, parse_mode=ParseMode.HTML
+                    , disable_web_page_preview="true")
+
+
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
 
@@ -22,7 +51,13 @@ def main():
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-# Log all errors
+    # Start
+    dp.add_handler(CommandHandler("start", start))
+
+    # Help
+    dp.add_handler(CommandHandler("help", help))
+
+    # Log all errors
     dp.add_error_handler(error)
 
     # Start the Bot
