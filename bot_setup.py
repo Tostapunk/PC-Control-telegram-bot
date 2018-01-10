17baf040af4a9d1bdf1f43ed26fc15e5169eac7e
Tostapunk <s.mattia2000@gmail.com>
2018-01-10 13:58:28 +0100

    bot_setup: add token check and a "quick view" to the requirements log

--- bot_setup.py
+++ bot_setup.py
@@ -1,28 +1,51 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import Tkinter, os
+import Tkinter, os, platform
 from Tkinter import *
 
 root = Tkinter.Tk()
 root.wm_title("Setup")
-root.geometry("230x245")
+root.geometry("290x400")
 root.resizable(width=False, height=False)
 
 def botfatherGET(val1):
-    file = open('botfather.txt', 'w')
-    file.write(val1)
-    file.close()
-    token1.delete(0, 'end')
+    if (len(val1) >= 45 and len(token1.get()) <= 50):
+        file = open('botfather.txt', 'w')
+        file.write(val1)
+        file.close()
+        token1.destroy()
+        B1.destroy()
+        L1_done.configure(text="Token saved!", font="TImes 11", fg="green", justify=LEFT)
+    elif len(token1.get()) == 0:
+        L1_done.configure(text="Your entry is empty", font="TImes 11", fg="red", justify=LEFT)
+    else:
+        L1_done.configure(text="The inserted token is wrong", font="TImes 11", fg="red", justify=LEFT)
 
 def imgurGET(val2):
-    file = open('imgur.txt', 'w')
-    file.write(val2)
-    file.close()
-    token2.delete(0, 'end')
+    if len(val2) != 0:
+        file = open('imgur.txt', 'w')
+        file.write(val2)
+        file.close()
+        token2.destroy()
+        B2.destroy()
+        L2_done.configure(text="Token saved!", font="TImes 11", fg="green", justify=LEFT)
+    else:
+        L2_done.configure(text="Your entry is empty", font="TImes 11", fg="red", justify=LEFT)
 
 def requirements():
     os.system("pip install -r requirements.txt > requirements_log.txt")
+    L3 = Label(root, text="The requirements install process is done.\n"
+                          "Do you want to take a look to the log?", justify=LEFT)
+    L3.pack()
+    B5 = Tkinter.Button(root, text="Yes", command=lambda: log_link())
+    B5.pack(pady=5)
+
+def log_link():
+    if platform.system() == "Windows":
+        os.system("requirements_log.txt")
+    else:
+        os.system("xdg-open requirements_log.txt")
 
 def create_mo_files():
     if os.path.isfile('locale/en/LC_MESSAGES/pccontrol.mo') is False:
@@ -35,24 +58,28 @@ def bot_start():
     create_mo_files()
     os.system("python bot.py")
 
-L1 = Label(root, text="BotFather token", justify=LEFT)
+L1 = Label(root, text="BotFather token", font="TImes 11 bold", justify=LEFT)
 L1.pack()
 token1 = Entry(root, bd =5)
 token1.pack()
-B1 = Tkinter.Button(root, text ="Confirm", command=lambda: botfatherGET(token1.get()))
+B1 = Tkinter.Button(root, text="Confirm", command=lambda: botfatherGET(token1.get()))
 B1.pack(pady=5)
+L1_done = Label(root, text="")
+L1_done.pack()
 
-L2 = Label(root, text="Imgur token", justify=LEFT)
+L2 = Label(root, text="Imgur token", font="TImes 11 bold", justify=LEFT)
 L2.pack()
 token2 = Entry(root, bd =5)
 token2.pack()
-B2 = Tkinter.Button(root, text ="Confirm", command=lambda: imgurGET(token2.get()))
+B2 = Tkinter.Button(root, text="Confirm", command=lambda: imgurGET(token2.get()))
 B2.pack(pady=5)
+L2_done = Label(root, text="")
+L2_done.pack()
 
-B3 = Tkinter.Button(root, text ="Install requirements", command=requirements)
+B3 = Tkinter.Button(root, text="Install requirements", command=requirements)
 B3.pack(pady=5)
 
-B4 = Tkinter.Button(root, text ="Start it!", command=bot_start)
+B4 = Tkinter.Button(root, text="Start it!", command=bot_start)
 B4.pack(pady=5)
 
 create_mo_files()
